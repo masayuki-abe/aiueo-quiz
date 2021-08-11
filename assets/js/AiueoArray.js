62740bd08252919cbff7579c2563b3b0441044a7
masayuki.abe <masayuki.abe@adminnoMacBook-Pro.local>
2021-08-11 13:27:47 +0900

    あいうえおの配列ファイルを作成

---
+++ assets/js/AiueoArray.js
@@ -0,0 +1,49 @@
+const aiueoArray = [
+  {
+    ja: 'あ',
+    en: 'a',
+    list: [
+      あひる,
+      あり,
+      あな
+    ]
+  },
+  {
+    ja: 'い',
+    en: 'i',
+    list: [
+      いるか,
+      いか,
+      いし
+    ]
+  },
+  {
+    ja: 'う',
+    en: 'u',
+    list: [
+      うし,
+      うま,
+      うさぎ
+    ]
+  },
+  {
+    ja: 'え',
+    en: 'e',
+    list: [
+      えんぴつ,
+      えのぐ,
+      えんとつ
+    ]
+  },
+  {
+    ja: 'お',
+    en: 'o',
+    list: [
+      おに,
+      おにぎり,
+      おの
+    ]
+  }
+]
+
+export default aiueoArray
