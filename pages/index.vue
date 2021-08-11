1277e1c9a8aa21da5baf2fd1a7a1f3dae5083e3c
masayuki.abe <masayuki.abe@adminnoMacBook-Pro.local>
2021-08-11 18:05:17 +0900

    配列からランダムに取得する処理を追加

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,48 @@
 <template>
-  <Tutorial />
+  <main>
+    <p>{{ question }}</p>
+    <p>{{ answer }}</p>
+    <p>{{ baseArrays }}</p>
+  </main>
 </template>
 
 <script>
-export default {}
+import aiueoArray from 'assets/js/AiueoArray'
+export default {
+  data () {
+    return {
+      baseArrays: [],
+      question: '',
+      answer: []
+    }
+  },
+  computed: {
+    // あいうえお配列のjsファイルを呼び出す
+    getBaseArray () {
+      return aiueoArray
+    },
+    // あいうえお配列からランダムで（配列の番号を）抽出する
+    getRandomArrayNumber () {
+      return Math.floor(Math.random() * this.getBaseArray.length)
+    }
+  },
+  mounted () {
+    this.importBaseArray()
+    this.getQuestion()
+    this.getAnswer()
+  },
+  methods: {
+    // computedで呼び出したあいうえお配列をdata変数に格納
+    importBaseArray () {
+      this.baseArrays = this.getBaseArray
+    },
+    // computedで抽出した配列番号のあいうえお配列のjaの値をdata変数に格納
+    getQuestion () {
+      this.question = this.baseArrays[this.getRandomArrayNumber].ja
+    },
+    getAnswer () {
+      this.answer = this.baseArrays.splice(this.getRandomArrayNumber, 1)
+    }
+  }
+}
 </script>
