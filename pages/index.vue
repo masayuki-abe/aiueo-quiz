2054123b827975f8351c974427e179f47c65cb0b
masayuki.abe <masayuki.abe@adminnoMacBook-Pro.local>
2021-08-12 13:41:51 +0900

    scriptの修正とssrをfalseに変更

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,13 @@
 <template>
-  <main>
-    <p>{{ question }}</p>
-    <p>{{ answer }}</p>
-    <p>{{ answerArray }}</p>
-  </main>
+  <div>
+    <p>Question：{{ question }}</p>
+    <ul>
+      <li>{{ answerArray }}</li>
+      <li v-for="notAnswerArray in notAnswerArrays" :key="notAnswerArray.en">
+        {{ notAnswerArray.list[randomNumber] }}
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
@@ -11,33 +15,38 @@ import aiueoArray from 'assets/js/AiueoArray'
 export default {
   data () {
     return {
-      question: '',
-      answer: []
+      getBaseArray: aiueoArray
     }
   },
   computed: {
     // あいうえお配列を呼び出し
-    getBaseArray () {
-      return aiueoArray
-    },
+    // getBaseArray () {
+    //   return aiueoArray
+    // },
     // あいうえお配列からランダムで（配列の番号を）抽出する
     getRandomArrayNumber () {
       return Math.floor(Math.random() * this.getBaseArray.length)
     },
-    // 抽出した配列番号の配列を取得
+    // 抽出した配列番号の配列からjaの値を取得
+    question () {
+      const question = this.getBaseArray[this.getRandomArrayNumber].ja
+      return question
+    },
+    // 抽出した配列番号の配列からlistの値を取得
     answerArray () {
-      const answerArray = this.getBaseArray[this.getRandomArrayNumber]
-      return answerArray
+      const answerArray = this.getBaseArray[this.getRandomArrayNumber].list
+      const randamAnswerArrayNumber = Math.floor(Math.random() * answerArray.length)
+      return answerArray[randamAnswerArrayNumber]
+    },
+    // 抽出した配列番号以外の配列を取得
+    notAnswerArrays () {
+      const notAnswerArrays = this.getBaseArray.filter((_, index) => index !== this.getRandomArrayNumber)
+      return notAnswerArrays
+    },
+    // 0-2の乱数
+    randomNumber () {
+      return Math.floor(Math.random() * 3)
     }
-    // computedで抽出した配列番号のあいうえお配列のjaの値をdata変数に格納
-    // getQuestion () {
-    //   this.question = this.baseArrays[this.getRandomArrayNumber].ja
-    //   return this.question
-    // },
-    // getAnswer () {
-    //   this.answer = this.baseArrays.filter((_, index) => index === this.getRandomArrayNumber)
-    //   return this.answer
-    // }
   }
 }
 </script>
