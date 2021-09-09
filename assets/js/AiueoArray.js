4ef62f8cf65a0fe3d0e3884d14ec5f39891c4b3e
masayuki-abe <cafenorthfox@gmail.com>
2021-09-10 00:01:46 +0900

    五十音追加中

--- assets/js/AiueoArray.js
+++ assets/js/AiueoArray.js
@@ -68,6 +68,552 @@ const aiueoArray = [
       'onigiri',
       'ono'
     ]
+  },
+  {
+    ja: 'か',
+    en: 'ka',
+    list: [
+      'かき',
+      'かえる',
+      'かきごおり'
+    ],
+    src: [
+      'kaki',
+      'kaeru',
+      'kakigori'
+    ]
+  },
+  {
+    ja: 'き',
+    en: 'ki',
+    list: [
+      'きりん',
+      'きつつき',
+      'きつね'
+    ],
+    src: [
+      'kirin',
+      'kitsutsuki',
+      'kitsune'
+    ]
+  },
+  {
+    ja: 'く',
+    en: 'ku',
+    list: [
+      'くるま',
+      'くつ',
+      'くり'
+    ],
+    src: [
+      'kuruma',
+      'kutsu',
+      'kuri'
+    ]
+  },
+  {
+    ja: 'け',
+    en: 'ke',
+    list: [
+      'けしごむ',
+      'けっこん',
+      'けーき'
+    ],
+    src: [
+      'keshigomu',
+      'kekkon',
+      'keki'
+    ]
+  },
+  {
+    ja: 'こ',
+    en: 'ko',
+    list: [
+      'こあら',
+      'こま',
+      'こおり'
+    ],
+    src: [
+      'koara',
+      'koma',
+      'kori'
+    ]
+  },
+  {
+    ja: 'さ',
+    en: 'sa',
+    list: [
+      'さる',
+      'さめ',
+      'さくらんぼ'
+    ],
+    src: [
+      'saru',
+      'same',
+      'sakuranbo'
+    ]
+  },
+  {
+    ja: 'し',
+    en: 'shi',
+    list: [
+      'しか',
+      'しまうま',
+      'した'
+    ],
+    src: [
+      'shika',
+      'shimauma',
+      'shita'
+    ]
+  },
+  {
+    ja: 'す',
+    en: 'su',
+    list: [
+      'すずめ',
+      'すいか',
+      'すいとう'
+    ],
+    src: [
+      'suzume',
+      'suika',
+      'suito'
+    ]
+  },
+  {
+    ja: 'せ',
+    en: 'se',
+    list: [
+      'せみ',
+      'せなか',
+      'せき'
+    ],
+    src: [
+      'semi',
+      'senaka',
+      'seki'
+    ]
+  },
+  {
+    ja: 'そ',
+    en: 'so',
+    list: [
+      'そり',
+      'そうじき',
+      'そーせーじ'
+    ],
+    src: [
+      'sori',
+      'sojiki',
+      'soseji'
+    ]
+  },
+  {
+    ja: 'た',
+    en: 'ta',
+    list: [
+      'たぬき',
+      'たおる',
+      'たつまき'
+    ],
+    src: [
+      'taoru',
+      'tanuki',
+      'tatsumaki'
+    ]
+  },
+  {
+    ja: 'ち',
+    en: 'chi',
+    list: [
+      'いるか',
+      'いか',
+      'いす'
+    ],
+    src: [
+      'iruka',
+      'ika',
+      'isu'
+    ]
+  },
+  {
+    ja: 'つ',
+    en: 'tsu',
+    list: [
+      'うし',
+      'うま',
+      'うさぎ'
+    ],
+    src: [
+      'ushi',
+      'uma',
+      'usagi'
+    ]
+  },
+  {
+    ja: 'て',
+    en: 'te',
+    list: [
+      'えんぴつ',
+      'えのぐ',
+      'えんとつ'
+    ],
+    src: [
+      'enpitsu',
+      'enogu',
+      'entotsu'
+    ]
+  },
+  {
+    ja: 'と',
+    en: 'to',
+    list: [
+      'おに',
+      'おにぎり',
+      'おの'
+    ],
+    src: [
+      'oni',
+      'onigiri',
+      'ono'
+    ]
+  },
+  {
+    ja: 'な',
+    en: 'na',
+    list: [
+      'あひる',
+      'あり',
+      'あいす'
+    ],
+    src: [
+      'ahiru',
+      'ari',
+      'aisu'
+    ]
+  },
+  {
+    ja: 'に',
+    en: 'ni',
+    list: [
+      'いるか',
+      'いか',
+      'いす'
+    ],
+    src: [
+      'iruka',
+      'ika',
+      'isu'
+    ]
+  },
+  {
+    ja: 'ぬ',
+    en: 'nu',
+    list: [
+      'うし',
+      'うま',
+      'うさぎ'
+    ],
+    src: [
+      'ushi',
+      'uma',
+      'usagi'
+    ]
+  },
+  {
+    ja: 'ね',
+    en: 'ne',
+    list: [
+      'えんぴつ',
+      'えのぐ',
+      'えんとつ'
+    ],
+    src: [
+      'enpitsu',
+      'enogu',
+      'entotsu'
+    ]
+  },
+  {
+    ja: 'の',
+    en: 'no',
+    list: [
+      'おに',
+      'おにぎり',
+      'おの'
+    ],
+    src: [
+      'oni',
+      'onigiri',
+      'ono'
+    ]
+  },
+  {
+    ja: 'は',
+    en: 'ha',
+    list: [
+      'あひる',
+      'あり',
+      'あいす'
+    ],
+    src: [
+      'ahiru',
+      'ari',
+      'aisu'
+    ]
+  },
+  {
+    ja: 'ひ',
+    en: 'hi',
+    list: [
+      'いるか',
+      'いか',
+      'いす'
+    ],
+    src: [
+      'iruka',
+      'ika',
+      'isu'
+    ]
+  },
+  {
+    ja: 'ふ',
+    en: 'fu',
+    list: [
+      'うし',
+      'うま',
+      'うさぎ'
+    ],
+    src: [
+      'ushi',
+      'uma',
+      'usagi'
+    ]
+  },
+  {
+    ja: 'へ',
+    en: 'he',
+    list: [
+      'えんぴつ',
+      'えのぐ',
+      'えんとつ'
+    ],
+    src: [
+      'enpitsu',
+      'enogu',
+      'entotsu'
+    ]
+  },
+  {
+    ja: 'ほ',
+    en: 'ho',
+    list: [
+      'おに',
+      'おにぎり',
+      'おの'
+    ],
+    src: [
+      'oni',
+      'onigiri',
+      'ono'
+    ]
+  },
+  {
+    ja: 'ま',
+    en: 'ma',
+    list: [
+      'あひる',
+      'あり',
+      'あいす'
+    ],
+    src: [
+      'ahiru',
+      'ari',
+      'aisu'
+    ]
+  },
+  {
+    ja: 'み',
+    en: 'mi',
+    list: [
+      'いるか',
+      'いか',
+      'いす'
+    ],
+    src: [
+      'iruka',
+      'ika',
+      'isu'
+    ]
+  },
+  {
+    ja: 'む',
+    en: 'mu',
+    list: [
+      'うし',
+      'うま',
+      'うさぎ'
+    ],
+    src: [
+      'ushi',
+      'uma',
+      'usagi'
+    ]
+  },
+  {
+    ja: 'め',
+    en: 'me',
+    list: [
+      'えんぴつ',
+      'えのぐ',
+      'えんとつ'
+    ],
+    src: [
+      'enpitsu',
+      'enogu',
+      'entotsu'
+    ]
+  },
+  {
+    ja: 'も',
+    en: 'mo',
+    list: [
+      'おに',
+      'おにぎり',
+      'おの'
+    ],
+    src: [
+      'oni',
+      'onigiri',
+      'ono'
+    ]
+  },
+  {
+    ja: 'ら',
+    en: 'ra',
+    list: [
+      'あひる',
+      'あり',
+      'あいす'
+    ],
+    src: [
+      'ahiru',
+      'ari',
+      'aisu'
+    ]
+  },
+  {
+    ja: 'り',
+    en: 'ri',
+    list: [
+      'いるか',
+      'いか',
+      'いす'
+    ],
+    src: [
+      'iruka',
+      'ika',
+      'isu'
+    ]
+  },
+  {
+    ja: 'る',
+    en: 'ru',
+    list: [
+      'うし',
+      'うま',
+      'うさぎ'
+    ],
+    src: [
+      'ushi',
+      'uma',
+      'usagi'
+    ]
+  },
+  {
+    ja: 'れ',
+    en: 're',
+    list: [
+      'えんぴつ',
+      'えのぐ',
+      'えんとつ'
+    ],
+    src: [
+      'enpitsu',
+      'enogu',
+      'entotsu'
+    ]
+  },
+  {
+    ja: 'ろ',
+    en: 'ro',
+    list: [
+      'おに',
+      'おにぎり',
+      'おの'
+    ],
+    src: [
+      'oni',
+      'onigiri',
+      'ono'
+    ]
+  },
+  {
+    ja: 'や',
+    en: 'ya',
+    list: [
+      'あひる',
+      'あり',
+      'あいす'
+    ],
+    src: [
+      'ahiru',
+      'ari',
+      'aisu'
+    ]
+  },
+  {
+    ja: 'ゆ',
+    en: 'yu',
+    list: [
+      'うし',
+      'うま',
+      'うさぎ'
+    ],
+    src: [
+      'ushi',
+      'uma',
+      'usagi'
+    ]
+  },
+  {
+    ja: 'よ',
+    en: 'yo',
+    list: [
+      'おに',
+      'おにぎり',
+      'おの'
+    ],
+    src: [
+      'oni',
+      'onigiri',
+      'ono'
+    ]
+  },
+  {
+    ja: 'わ',
+    en: 'wa',
+    list: [
+      'あひる',
+      'あり',
+      'あいす'
+    ],
+    src: [
+      'ahiru',
+      'ari',
+      'aisu'
+    ]
   }
 ]
 
